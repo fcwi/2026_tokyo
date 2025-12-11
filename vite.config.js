30754e14d471c167a6ebca7a8c89ea2ec1eb19a8
unknown <fcwi@holtek.com.tw>
2025-12-11 10:56:01 +0800

    Update config for 2026_tokyo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2026_tokyo/',
+  base: '/2026_tokyo/',  // 👈 這裡一定要改成這樣！
 })
