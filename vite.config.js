eeec0187930516e93f0ff66a9540b91c56b168b7
fcwi <wadashihamin@gmail.com>
2026-01-19 14:32:40 +0800

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -112,7 +112,7 @@ export default defineConfig({
     minify: 'terser',
     terserOptions: {
       compress: {
-        drop_console: false, // 如果要保留 console.log以便除錯，請設為 false
+        drop_console: true, // 如果要保留 console.log以便除錯，請設為 false
         drop_debugger: true, // 移除 debugger
       },
     },
